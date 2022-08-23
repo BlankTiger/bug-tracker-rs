be804725a5e15a0680112e9c1f22141271aa0edc
Maciej Urban <maciej-urban@outlook.com>
2022-08-23 19:05:13 +0200

    login redirect kind of working, browser says its somehow wrong

--- actix-backend/src/main.rs
+++ actix-backend/src/main.rs
@@ -1,5 +1,11 @@
+mod auth;
+mod utils;
+mod routes;
+use routes::*;
 use actix_files::Files;
 use actix_web::{get, web, App, HttpServer, Responder};
+use actix_web_lab::middleware::from_fn;
+use auth::manage::reject_not_authenticated;
 use rand::prelude::*;
 
 #[get("/random/{min}-{max}")]
@@ -14,6 +20,8 @@ async fn main() -> std::io::Result<()> {
     const ACTIX_PORT: &str = std::env!("ACTIX_PORT");
     HttpServer::new(|| {
         App::new()
+            .wrap(from_fn(reject_not_authenticated))
+            .route("/login", web::get().to(login::login_form))
             .service(random)
             .service(Files::new("/", "../yew-frontend/dist").index_file("index.html"))
     })
