fe172eb1c1b0ac058edc32a710f89217d0c11f93
Maciej Urban <maciej-urban@outlook.com>
2022-08-23 20:54:11 +0200

    scoped denial of service without authorization to /

--- actix-backend/src/main.rs
+++ actix-backend/src/main.rs
@@ -1,12 +1,12 @@
 mod auth;
-mod utils;
 mod routes;
-use routes::*;
+mod utils;
 use actix_files::Files;
 use actix_web::{get, web, App, HttpServer, Responder};
 use actix_web_lab::middleware::from_fn;
 use auth::manage::reject_not_authenticated;
 use rand::prelude::*;
+use routes::*;
 
 #[get("/random/{min}-{max}")]
 async fn random(args: web::Path<(i32, i32)>) -> impl Responder {
@@ -19,11 +19,13 @@ async fn random(args: web::Path<(i32, i32)>) -> impl Responder {
 async fn main() -> std::io::Result<()> {
     const ACTIX_PORT: &str = std::env!("ACTIX_PORT");
     HttpServer::new(|| {
-        App::new()
-            .wrap(from_fn(reject_not_authenticated))
-            .route("/login", web::get().to(login::login_form))
-            .service(random)
-            .service(Files::new("/", "../yew-frontend/dist").index_file("index.html"))
+        App::new().service(
+            web::scope("/")
+                .wrap(from_fn(reject_not_authenticated))
+                .route("/login", web::get().to(login::login_form))
+                .service(random)
+                .service(Files::new("/", "../yew-frontend/dist").index_file("index.html")),
+        )
     })
     .bind(("127.0.0.1", ACTIX_PORT.parse::<u16>().unwrap_or(8080)))?
     .run()
