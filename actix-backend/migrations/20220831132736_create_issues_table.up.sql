78c9d23f0a1ee11ff50a9a20a2b636d441887af8
Maciej Urban <maciej-urban@outlook.com>
2022-08-31 15:31:28 +0200

    Changed database schema
    Changes:
    - projects now have status too
    - created issues table

---
+++ actix-backend/migrations/20220831132736_create_issues_table.up.sql
@@ -0,0 +1,11 @@
+-- Add up migration script here
+create table issues (
+	issue_id serial primary key,
+	issue_name text not null,
+	desc_short text not null,
+	desc_long text,
+	status text not null,
+	assigned_to uuid[] not null,
+	created_by uuid not null references(user_id),
+	created_at timestamp not null default now()
+);
